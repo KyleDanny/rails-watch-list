6b820c95a6ea6dffccb138593dadc22981cf589c
Kyle Danny <codingkyle92@gmail.com>
2021-05-12 13:52:35 +0200

    reseeded database, push to heroku, still not working

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
+  new Typed('#typed-text', {
     strings: ["Create your unique database"],
     typeSpeed: 75,
     loop: true
